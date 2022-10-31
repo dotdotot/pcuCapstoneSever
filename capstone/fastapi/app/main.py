af9be55c31d38a1e30c0ab5660be0d5f4439393e
gksekdud02 <gksekdud02@naver.com>
2022-11-01 01:26:35 +0900

    2022.11.01

--- capstone/fastapi/app/main.py
+++ capstone/fastapi/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from requests import Session
 from fastapi.params import Depends
 from typing import List
@@ -6,7 +6,10 @@ from starlette.responses import RedirectResponse
 from consts import JWT_SECRET, JWT_ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_EXPIRE_MINUTES
 from datetime import datetime, timedelta
 from starlette.responses import JSONResponse
+from sqlalchemy import desc
 
+import json
+import urllib.request
 import bcrypt
 import models, schemas
 import uvicorn
@@ -24,6 +27,19 @@ def get_db():
         yield db
     finally:
         db.close()
+
+#IP주소 국가코드가 KR이면 True
+def IP(request: Request):
+    IP = request.client.host
+    key = 'B1gJBJxC7p%2F%2BN9qGjgKEi%2FU6LePtHCPxltLYo7I7%2B2%2FIu1Jei5VYqg%2B8MIwp2BWODMsoOe7hc9th8kW5A4aZew%3D%3D'
+    URL = 'http://apis.data.go.kr/B551505/whois/ipas_country_code?serviceKey='+ key + '&query='+ IP +'&answer=json'
+    json_page = urllib.request.urlopen(URL)
+    json_data = json_page.read().decode("utf-8")
+    json_array = json.loads(json_data)
+    contrycode = json_array.get("response").get("whois").get("countryCode")
+    if contrycode == "KR":
+        return True
+
 
 @app.get("/")
 def main():
@@ -101,7 +117,7 @@ def create_refresh_token(*, data: dict, expires_delta: int = None):
 
 # 특정방 찾기
 @app.get("/findRoomInfo/{room_name}",status_code=200)
-async def find_room(room_name:str,db:Session=Depends(get_db)):
+async def find_room(room_name:str,db:Session=Depends(get_db)):    
     q = db.query(models.RoomList.id,
                 models.RoomList.created_at,
                 models.RoomList.room_name,
@@ -134,13 +150,17 @@ def add_room(room_name:str, entrada: schemas.Room, db:Session = Depends(get_db))
 
 # id에 해당하는 방 목록
 @app.get("/findRoom/{login_id}",status_code=200)
-def find_room(login_id:str,db:Session=Depends(get_db)):
-    db_user_id = db.query(models.User.id).filter_by(login_id=login_id).scalar_subquery()
-    print(db_user_id)
-    room = db.query(models.RoomList.room_name).filter_by(user_id=db_user_id).all()
-    print(room)
+def find_room(login_id:str,request: Request,db:Session=Depends(get_db)):
+    KR_IP = IP(request)
+    print(KR_IP)
+    if KR_IP == True:
+        db_user_id = db.query(models.User.id).filter_by(login_id=login_id).scalar_subquery()
+        room = db.query(models.RoomList.room_name).filter_by(user_id=db_user_id).all()
 
-    return room
+        return room
+    else:
+        return JSONResponse(status_code=400,content=dict(msg="해외 IP주소 차단"))
+
 
 
 # 모든 방에 대한 정보
@@ -191,11 +211,11 @@ async def delete_room(room_name:str, db:Session=Depends(get_db)):
 
 
 #홈화면
-@app.get("/home/{login_id}",response_model=schemas.Room) #방이름도 입력받아야함 / 제일 최근 정보로 나타내야 함 -> 수정
+@app.get("/home/{login_id}",response_model=schemas.Room) # 하드웨어에서 현재위치를 받아 그 위치의 가장 최근 정보 전달-> 수정해야 함
 def home_info(login_id:str, db:Session=Depends(get_db)):
     db_user_id = db.query(models.User.id).filter_by(login_id=login_id).scalar_subquery()
     db_room_id = db.query(models.RoomList.id).filter_by(user_id=db_user_id,room_name="tt").scalar_subquery()
-    room_info = db.query(models.Room_Management).filter_by(room_id=db_room_id).first()
+    room_info = db.query(models.Room_Management).filter_by(room_id=db_room_id).order_by(desc(models.Room_Management.created_at)).first()
 
     return room_info
 
